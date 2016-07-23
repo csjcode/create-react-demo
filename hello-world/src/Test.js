9dc0cefa3a9f2a8426e0db380ffaae05c059e9dc
csjcode <christophersstjohn@gmail.com>
2016-07-23 14:49:22 +0700

    Adding test component

---
+++ hello-world/src/Test.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Test extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Hello Test</h2>
+        </div>
+        <p className="App-intro">
+          Just trying out a new component.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Test;
